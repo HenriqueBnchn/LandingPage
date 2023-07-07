375c3bc94c75b1840c6d02982dd0c040436a7ab5
Henrique Bnchn <henriqueoze@gmail.com>
2023-07-07 15:35:25 -0400

    Alterações geral e animações rolagem

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const root = document.querySelector(':root')
 const btn_switch_on = document.querySelector(".on")
 const btn_switch_off = document.querySelector(".off")
 
+const header = document.querySelector(".header")
+const hero = document.querySelector(".hero_div")
+
 const sobremim_option = document.querySelector('#sobremim_option')
 const habilidades_option = document.querySelector('#habilidades_option')
 const hobbies_option = document.querySelector('#hobbies_option')
@@ -15,8 +18,7 @@ const social_icons = document.querySelectorAll('.social svg')
 
 
 btn_switch_on.addEventListener('click', () => {
-  root.style.setProperty('--luminance', 1);
-  root.style.setProperty('--text-black', '100%');
+  root.classList.remove('light')
   btn_switch_on.style.display = 'none'
   btn_switch_off.style.display = 'block'
   social_icons.forEach(icon => {
@@ -25,8 +27,7 @@ btn_switch_on.addEventListener('click', () => {
 })
 
 btn_switch_off.addEventListener('click', () => {
-  root.style.setProperty('--luminance', 0);
-  root.style.setProperty('--text-black', '1%');
+  root.classList.add('light')
   btn_switch_off.style.display = 'none'
   btn_switch_on.style.display = 'block'
   social_icons.forEach(icon => {
@@ -39,12 +40,15 @@ sobremim_option.addEventListener('click', () => {
   sobremim_option.classList.add(ativo)
   habilidades_option.classList.remove(ativo)
   hobbies_option.classList.remove(ativo)
+
+  scrollTo(0, 900)
 })
 
 habilidades_option.addEventListener('click', () => {
   sobremim_option.classList.remove(ativo)
   habilidades_option.classList.add(ativo)
   hobbies_option.classList.remove(ativo)
+  scrollTo(0, 1750)
 })
 
 hobbies_option.addEventListener('click', () => {
@@ -53,10 +57,26 @@ hobbies_option.addEventListener('click', () => {
   hobbies_option.classList.add(ativo)
 })
 
-// header_options.map(option => {
-//   option.addEventListener('click', function(){
-//     if(option.classList.contains('text_active')){
-//       console.log('dsadasdas')
-//     }
-//   })
-// })
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 50;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", function () {
+  header.classList.toggle("fixed", window.scrollY > 100)
+  hero.classList.toggle("margintop", window.scrollY > 100)
+})
+
+
+window.addEventListener("scroll", reveal);
