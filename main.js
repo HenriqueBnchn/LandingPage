683fe34daa6f2fb2971bcf77aa36848d03c7e1c4
Henrique Bnchn <henriqueoze@gmail.com>
2023-07-12 14:19:17 -0400

    Adicionado aba contatos e alterado espaçamentos

--- main.js
+++ main.js
@@ -9,20 +9,25 @@ const btn_switch_off = document.querySelector(".off")
 const header = document.querySelector(".header")
 const hero = document.querySelector(".hero_div")
 
+const home_option = document.querySelector('#home_option')
+const hero_div = document.querySelector('.hero_div')
+
 const sobremim_option = document.querySelector('#sobremim_option')
 const sobre_mim_div = document.querySelector('.sobre_mim_div')
+const sobremim_h = sobre_mim_div.getBoundingClientRect().top - 120
 
 const habilidades_option = document.querySelector('#habilidades_option')
 const habilidades_div = document.querySelector('.habilidades_div')
-
+const habilidades_h = habilidades_div.getBoundingClientRect().top - 100
 
 const hobbies_option = document.querySelector('#hobbies_option')
 const hobbies_div = document.querySelector('.hobbies_div')
-const hobbies_y = (hobbies_div.getBoundingClientRect().top - 100)
-
+const hobbies_h = hobbies_div.getBoundingClientRect().top - 150
 
 const social_icons = document.querySelectorAll('.social svg')
 
+
+////////////////////////////////////////////// trocar cor geral //////////////////////////////////////////
 
 btn_switch_on.addEventListener('click', () => {
   root.classList.remove('light')
@@ -44,20 +49,25 @@ btn_switch_off.addEventListener('click', () => {
 })
 
 
+////////////////////////////////////////////// opcoes rolar//////////////////////////////////////////
+home_option.addEventListener('click', () => {
+  window.scrollTo({top: 0})
+})
+
 sobremim_option.addEventListener('click', () => {
-  let h = (sobre_mim_div.getBoundingClientRect().top - 50)
-  window.scrollTo({top: h})
+  window.scrollTo({top: sobremim_h})
 })
 
 habilidades_option.addEventListener('click', () => {
-  let h = (habilidades_div.getBoundingClientRect().top - 50)
-  window.scrollTo({top: h})
+  window.scrollTo({top: habilidades_h})
 })
 
 hobbies_option.addEventListener('click', () => {
-  let h = (hobbies_div.getBoundingClientRect().top - 50)
-  window.scrollTo({top: h})
+  window.scrollTo({top: hobbies_h})
 })
+
+
+////////////////////////////////////////////// revelar texto //////////////////////////////////////////
 
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
